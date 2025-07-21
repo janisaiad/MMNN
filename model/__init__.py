e0262a755d0ee490eadf8cb92e17b52e99269e58
janisaiad <janisaiad.ja@gmail.com>
2025-07-21 19:10:13 -0400

    added sin in ntk infinite

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
 from .mmnn import MMNN
 from .mmnn_jax import MMNNJax
-from .ntk import compute_ntk_nngp_recursive, relu, relu_dot
+from .ntk import compute_ntk_nngp_recursive, relu, relu_dot, sin, sin_dot
 
 __all__ = ["MMNN", "MMNNJax", "ntk_infinite"]
